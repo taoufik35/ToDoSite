d5f05118e0459dc7e1d1490723394fbb027084bb
taoufik35 <79227708+taoufik35@users.noreply.github.com>
2021-07-19 02:48:10 +0200

    doc and button user

--- public/js/main.js
+++ public/js/main.js
@@ -9,10 +9,14 @@ btn.onclick = (() => {
 
 
 
-$("#all ul li").hover(function() { // Mouse over
-    $(this).siblings().stop().fadeTo(300, 0.6);
-    $(this).parent().siblings().stop().fadeTo(300, 0.3); 
-  }, function() { // Mouse out
-    $(this).siblings().stop().fadeTo(300, 1);
-    $(this).parent().siblings().stop().fadeTo(300, 1);
-  });
+// /----------------------modal instruction--------------------------------/
+
+
+
+
+
+let consigne = "Lorem ipsum dolorsit amet consectetur adipisicing elit. Sunt accusantium tempora, iste ad ea reiciendis deleniti enim neque,excepturi tempore molestiae consectetur cupiditate impedit et in alias aliquid illum quidem! Perferendis doloribus beatae optio eos obcaecati omnis quidem delectus molestiae autem quibusdam magnam ab sunt fugit, tempora soluta alias veniam dicta explicabo cupiditate consequuntur! Suscipit porro,iusto esse unde ullam modi et magnam in officia voluptates quasi illo excepturi ea,aliquid dolorum facere fuga ipsa ipsam quaerat culpa eos. Laborum ullam maxime doloribus"
+
+function securmess() {
+  alert(consigne);
+}
